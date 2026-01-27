1162f990facec6f801579046a1af5cf4f57b82f4
Trond Kandal <Trond.Kandal@ntnu.no>
2026-01-27 15:51:47 +0100

    Changes Go doc.

--- models/fsgraphql/sprak.go
+++ models/fsgraphql/sprak.go
@@ -4,7 +4,7 @@ package fsgraphql
  * Copyright (c) 2026 Norwegian University of Science and Technology, Trondheim, Norway
  */
 
-// NavnAlleSprak is the model for names of organisation units.
+// NavnAlleSprak is the model for names where it is possible to query for names.
 type NavnAlleSprak struct {
 	EN string `json:"en"`
 	NN string `json:"nn"`
