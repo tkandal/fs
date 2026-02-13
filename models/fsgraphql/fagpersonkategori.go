4e5fb543b9374bce08263b6e681a1b181bd072e6
Trond Kandal <Trond.Kandal@ntnu.no>
2026-02-13 14:38:25 +0100

    Added model for staff categories.

---
+++ models/fsgraphql/fagpersonkategori.go
@@ -0,0 +1,20 @@
+package fsgraphql
+
+/*
+ * Copyright (c) 2026 Norwegian University of Science and Technology, Trondheim, Norway
+ */
+
+// Fagpersonkategori is model for faculty staff categories.
+type Fagpersonkategori struct {
+	ID            string `json:"id"   graphql:"id"`
+	Kode          string `json:"kode" graphql:"kode"`
+	NavnAlleSprak struct {
+		UND string `json:"und" graphql:"und"`
+	}
+}
+
+type FagpersonkategoriResponse struct {
+	Fagpersonkategorier struct {
+		Nodes []Fagpersonkategori `json:"nodes"`
+	} `json:"fagpersonkategorier"`
+}
